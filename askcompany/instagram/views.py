91adf50d01f22b19e0147e585d44dcab19c9f5f6
Changmin Lee <ckdals8073@gmail.com>
2022-01-12 00:56:04 +0900

    add featrue that search post by query

--- askcompany/instagram/views.py
+++ askcompany/instagram/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
+def post_list(request) : 
+    qs = Post.objects.all()
+    q = request.GET.get('q', '')
+    if q : 
+        qs = qs.filter(message__icontains=q)
+    return render(
+        # render를 통해 django의 template를 이용해 손쉽게 만듦
+        request, 
+        # instagram/templates/instagram/post_list.html을 지정하는 것임
+        'instagram/post_list.html' ,
+        {
+            'post_list' : qs,
+        }
+    )
