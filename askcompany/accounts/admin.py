52ad7db166f492e3aca421fc9aee8d1b50106c27
Changmin Lee <ckdals8073@gmail.com>
2022-01-22 16:11:19 +0900

    add new model named Profile on accounts app

--- askcompany/accounts/admin.py
+++ askcompany/accounts/admin.py
@@ -1,3 +1,7 @@
+from .models import Profile
 from django.contrib import admin
 
 # Register your models here.
+@admin.register(Profile)
+class ProgileAdmin(admin.ModelAdmin) : 
+    list_display = ["user", "address", "zip_code"]
--- askcompany/accounts/models.py
+++ askcompany/accounts/models.py
@@ -1,3 +1,8 @@
+from django.conf import settings
 from django.db import models
 
 # Create your models here.
+class Profile(models.Model) : 
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    address = models.CharField(max_length=100)
+    zip_code = models.CharField(max_length=6)
